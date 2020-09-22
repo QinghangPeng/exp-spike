dffa0c5f3d803d6a0fdbea6d189637e6d31472f1
JackSon <voyagejk.peng@gmail.com>
2020-09-22 23:01:09 +0800

    :sparkles:商品详情页

--- src/main/java/com/exp/spike/dao/GoodsDao.java
+++ src/main/java/com/exp/spike/dao/GoodsDao.java
@@ -23,4 +23,9 @@ public interface GoodsDao {
             "left join goods g on mg.goods_id = g.id")
     List<GoodsVo> listGoodsVo();
 
+    @Select("select g.*,mg.stock_count,mg.miaosha_price,mg.start_date,mg.end_date " +
+            "from miaosha_goods mg " +
+            "left join goods g on mg.goods_id = g.id " +
+            "where g.id = #{goodsId}")
+    GoodsVo getGoodsVoByGoodsId(@Param("goodsId") Long goodsId);
 }
--- src/main/java/com/exp/spike/service/GoodsService.java
+++ src/main/java/com/exp/spike/service/GoodsService.java
@@ -35,8 +35,45 @@ public class GoodsService {
         return "goods_list";
     }
 
+    public String toDetail(Model model, MiaoshaUser user, Long goodsId) {
+        model.addAttribute("user",user);
+        GoodsVo goods = getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods",goods);
+
+        //
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        //秒杀状态
+        int miaoshaStatus = 0;
+        //秒杀剩余时间
+        int remainSeconds = 0;
+        if (now < startAt) {
+            //秒杀还未开始,倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int) (startAt - now) / 1000;
+        } else if (now > endAt) {
+            //秒杀已结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        } else {
+            //秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("miaoshaStatus",miaoshaStatus);
+        model.addAttribute("remainSeconds",remainSeconds);
+        return "goods_detail";
+    }
+
     public List<GoodsVo> listGoodsVo() {
         return goodsDao.listGoodsVo();
     }
+
+    public GoodsVo getGoodsVoByGoodsId(Long goodsId) {
+        return goodsDao.getGoodsVoByGoodsId(goodsId);
+    }
+
 
 }
